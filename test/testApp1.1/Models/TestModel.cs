24df8998f886257c0d128eb9aa7369eb28c52132
Ondrej Morsky <ondrej.morsky@solarwinds.com>
2017-06-23 10:17:43 +0200

    .NET Core 1.1 test app

---
+++ test/testApp1.1/Models/TestModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace testApp.Models
+{
+	public class TestModel
+	{
+		public int Integer { get; set; }
+		public string String { get; set; }
+		public float Float { get; set; }
+		public bool Boolean { get; set; }
+
+		[JsonProperty("NewName")]
+		public string Renamed { get; set; }
+		public SubModel SubModel { get; set; }
+		
+	}
+
+	public class SubModel
+	{
+		public string Value1 { get; set; }
+		public string Value2 { get; set; }
+		public int[] Numbers { get; set; }
+	}
+}
---
+++ test/testApp1.1/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Morcatko.AspNetCore.JsonMergePatch;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace testApp
+{
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+		public IConfigurationRoot Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+			services.AddJsonMergePatch();
+			services.AddSwaggerGen(c =>
+				{
+					c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+				});
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
+
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+			});
+			app.UseMvc();
+		}
+	}
+}
