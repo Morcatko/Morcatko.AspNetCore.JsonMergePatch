33e2dd54d0fa6686b2e8b24f44f12997e7d290ab
Ondrej Morsky <ondrej.morsky@solarwinds.com>
2017-06-23 09:09:31 +0200

    Migrate to csproj

--- test/testApp/Startup.cs
+++ test/testApp/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Morcatko.AspNetCore.JsonMergePatch;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace testApp
 {
@@ -20,22 +21,26 @@ namespace testApp
 
 		public IConfigurationRoot Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddSwaggerGen();
 			services.AddMvc();
 			services.AddJsonMergePatch();
+			services.AddSwaggerGen(c =>
+				{
+					c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+				});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
 			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
 			loggerFactory.AddDebug();
 
 			app.UseSwagger();
-			app.UseSwaggerUi();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+			});
 			app.UseMvc();
 		}
 	}
