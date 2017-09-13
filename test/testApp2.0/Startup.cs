3e33028289a8ad54b63108b25eb18004817bab6a
Ondra <ondrej.morsky@hotmail.com>
2017-09-13 13:32:54 +0200

    Update to netstandard 2.0

---
+++ test/testApp2.0/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Morcatko.AspNetCore.JsonMergePatch;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace testApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddJsonMergePatch();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            app.UseMvc();
+        }
+    }
+}
