5b93ecf1afcb9e0cbfb941b6ba2d6ac698759f0e
Ondra <ondrej.morsky@hotmail.com>
2019-09-06 08:42:31 +0200

    Preview9

--- dotnet-pack-3.ps1
+++ dotnet-pack-3.ps1
@@ -3,9 +3,9 @@ Param (
 [ValidateNotNull()]
 $versionSuffix)
 
-& dotnet pack "$PSScriptRoot\src\3.0-JsonMergePatch" --configuration Release --version-suffix $versionSuffix
+& dotnet pack "$PSScriptRoot\src\3.0-JsonMergePatch.Document" --configuration Release --version-suffix $versionSuffix
 & dotnet pack "$PSScriptRoot\src\3.0-JsonMergePatch.NewtonsoftJson" --configuration Release --version-suffix $versionSuffix
 
 New-Item -ItemType Directory -Force -Path "$PSScriptRoot\.nugets"
-& copy "$PSScriptRoot\src\3.0-JsonMergePatch\bin\Release\*.*nupkg" "$PSScriptRoot\.nugets"
+& copy "$PSScriptRoot\src\3.0-JsonMergePatch.Document\bin\Release\*.*nupkg" "$PSScriptRoot\.nugets"
 & copy "$PSScriptRoot\src\3.0-JsonMergePatch.NewtonsoftJson\bin\Release\*.*nupkg" "$PSScriptRoot\.nugets"
