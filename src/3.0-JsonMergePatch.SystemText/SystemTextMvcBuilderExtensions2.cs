a3b10e397188f2163e21c2f629c81c85b0ae9b92
Eric Boumendil <eric.bml@gmail.com>
2020-03-21 18:59:09 +0100

    IServiceCollection extensions namespace Morcatko.AspNetCore.JsonMergePatch

---
+++ src/3.0-JsonMergePatch.SystemText/SystemTextMvcBuilderExtensions2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using System;
+
+// Extensions in Morcatko.AspNetCore.JsonMergePatch.SystemText kept for retro-compatibility.
+
+namespace Morcatko.AspNetCore.JsonMergePatch.SystemText
+{
+	public static class SystemTextMvxBuilderExtensions
+	{
+        public static IMvcBuilder AddSystemTextJsonMergePatch(this IMvcBuilder builder, Action<JsonMergePatchOptions> configure = null)
+            => JsonMergePatch.SystemTextMvxBuilderExtensions.AddSystemTextJsonMergePatch(builder, configure);
+
+        public static IMvcCoreBuilder AddSystemTextJsonMergePatch(this IMvcCoreBuilder builder, Action<JsonMergePatchOptions> configure = null)
+            => JsonMergePatch.SystemTextMvxBuilderExtensions.AddSystemTextJsonMergePatch(builder, configure);
+    }
+}
