a6556fbaa517394a52cc00114c54bb9510909079
Eric Boumendil <eric.bml@gmail.com>
2020-03-23 19:25:01 +0100

    Added ObsoleteAttribute

--- src/3.0-JsonMergePatch.SystemText/SystemTextMvcBuilderExtensions2.cs
+++ src/3.0-JsonMergePatch.SystemText/SystemTextMvcBuilderExtensions2.cs
@@ -5,15 +5,16 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using System;
 
-// Extensions in Morcatko.AspNetCore.JsonMergePatch.SystemText kept for retro-compatibility.
-
 namespace Morcatko.AspNetCore.JsonMergePatch.SystemText
 {
+    [Obsolete("Moved to namespace Morcatko.AspNetCore.JsonMergePatch")]
 	public static class SystemTextMvxBuilderExtensions
 	{
+        [Obsolete("Moved to namespace Morcatko.AspNetCore.JsonMergePatch")]
         public static IMvcBuilder AddSystemTextJsonMergePatch(this IMvcBuilder builder, Action<JsonMergePatchOptions> configure = null)
             => JsonMergePatch.SystemTextMvxBuilderExtensions.AddSystemTextJsonMergePatch(builder, configure);
 
+        [Obsolete("Moved to namespace Morcatko.AspNetCore.JsonMergePatch")]
         public static IMvcCoreBuilder AddSystemTextJsonMergePatch(this IMvcCoreBuilder builder, Action<JsonMergePatchOptions> configure = null)
             => JsonMergePatch.SystemTextMvxBuilderExtensions.AddSystemTextJsonMergePatch(builder, configure);
     }
